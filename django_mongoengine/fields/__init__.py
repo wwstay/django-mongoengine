dab0857550280e46f248988d82f958423ef20723
Sergey Tereschenko <serg.partizan@gmail.com>
2017-06-16 08:44:42 +0300

    test fix: patch every class in mongoengine _class_registry_cache

--- django_mongoengine/fields/__init__.py
+++ django_mongoengine/fields/__init__.py
@@ -1,4 +1,8 @@
+from mongoengine import fields
+from mongoengine import common
+
 from . import djangoflavor
+
 
 def init_module():
     """
@@ -6,17 +10,16 @@ def init_module():
     use DjangoField mixin as default
     """
     import sys
-    from mongoengine import fields
     current_module = sys.modules[__name__]
     current_module.__all__ = fields.__all__
 
     for name in fields.__all__:
         fieldcls = getattr(fields, name)
         mixin = getattr(djangoflavor, name, djangoflavor.DjangoField)
-        setattr(
-            current_module, name,
-            type(name, (mixin, fieldcls), {})
-        )
+        patched_cls = type(name, (mixin, fieldcls), {})
+        setattr(current_module, name, patched_cls)
+        common._class_registry_cache[name] = patched_cls
+
 init_module()
 
 def patch_mongoengine_field(field_name):
@@ -25,11 +28,10 @@ def patch_mongoengine_field(field_name):
     becouse it's required in django.forms.models.fields_for_model
     importing using mongoengine internal import cache
     """
-    from mongoengine import common
     field = common._import_class(field_name)
     for k in ["__eq__", "__lt__", "__hash__", "attname"]:
         if not k in field.__dict__:
             setattr(field, k, djangoflavor.DjangoField.__dict__[k])
 
-for f in ["StringField", "ObjectIdField"]:
-    patch_mongoengine_field(f)
+#for f in ["StringField", "ObjectIdField"]:
+#    patch_mongoengine_field(f)
